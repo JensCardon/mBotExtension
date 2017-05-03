b833aa6274ba400f61c4eecfaf3c9fbc25898f25
Jens <jecardon@vub.ac.be>
2017-05-03 17:14:16 +0200

    l

--- src/extensions/mbot/mbot.js
+++ src/extensions/mbot/mbot.js
@@ -585,24 +585,7 @@
         if(!device) return {status: 1, msg: 'Controller disconnected'};
         return {status: 2, msg: 'Controller connected'};
     }
-
-    // Converts a byte into a value of the range -1 -> 1 with two decimal places of precision
-    function convertByteStr(byte) { return (parseInt(byte, 16) - 128) / 128; }
-    ext.readJoystick = function(name) {
-        var retval = null;
-        switch(name) {
-            case 'leftX': retval = convertByteStr(input[12] + input[13]); break;
-            case 'leftY': retval = -convertByteStr(input[14] + input[15]); break;
-            case 'rightX': retval = convertByteStr(input[16] + input[17]); break;
-            case 'rightY': retval = -convertByteStr(input[18] + input[19]); break;
-        }
-
-        // If it's hardly off center then treat it as centered
-        if(Math.abs(retval) < 0.1) retval = 0;
-
-        return retval.toFixed(2);
-    }
-
+    
 	ext.sendmBot = function(connectionId){
 
 	}
